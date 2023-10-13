ff2074a1b39db509b331793bbf13172a8b72e29e
AnhTuan <1951052223tuan@ou.edu.vn>
2023-10-13 23:31:45 +0700

    orm create user

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import mysql from 'mysql2/promise';
 import bluebird from 'bluebird';
+import db from '../models/index';
 
 
 
@@ -10,14 +11,19 @@ const hashPassword = (userPassword) => {
     return bcrypt.hashSync(userPassword, salt);
 }
 
-const createUser = async (email, password, username) => { 
+const createUser = async (email, password, username) => {
     let hashPass = hashPassword(password);
-    const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'nodejs_reactjs', Promise: bluebird});
-
-    const [rows, fields] =
-        await connection.execute('INSERT INTO users (email, password, username) VALUES (?, ?, ?)',
-            [email, hashPass, username]
-        );
+    
+    try {
+        await db.User.create({
+            username: username,
+            email: email,
+            password: hashPass
+        })
+    } catch (err) { 
+        console.log('check error: ', err)
+    }
+        
 }
 
 const userList = async () => {

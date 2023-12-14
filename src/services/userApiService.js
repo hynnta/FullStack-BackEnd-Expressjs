56175791b70add3a596263f0def3d321c5617c3d
AnhTuan <1951052223tuan@ou.edu.vn>
2023-12-15 00:55:31 +0700

    complete api to delete user

--- src/services/userApiService.js
+++ src/services/userApiService.js
@@ -1,60 +1,74 @@
 import db from "../models";
 
-const getAllUser = async () => { 
-    
-    try {
-        let users = await db.User.findAll({
-            attributes: ["id", "username", "email"],
-            include: {model: db.Group, attributes: [ "name", "description"]},
-            
-        });
-        
-        
-        if (users) {
-            
-            console.log('check User: ', users);
-            
-            return {
-                EM: 'get data success',
-                EC: 0,
-                DT: users
-                
-            }
-            
-        } else { 
-            
-            return {
-                EM: 'not found users',
-                EC: 1,
-                DT: []
-            }
-        }
-        
-    } catch (error) {
-        console.log(error);
-        return {
-                EM: 'something wrong with services',
-                EC: 1,
-                DT: []
-            }
+const getAllUser = async () => {
+  try {
+    let users = await db.User.findAll({
+      attributes: ["id", "username", "email"],
+      include: { model: db.Group, attributes: ["name", "description"] },
+    });
+
+    if (users) {
+      console.log("check User: ", users);
+
+      return {
+        EM: "get data success",
+        EC: 0,
+        DT: users,
+      };
+    } else {
+      return {
+        EM: "not found users",
+        EC: 1,
+        DT: [],
+      };
     }
-}
+  } catch (error) {
+    console.log(error);
+    return {
+      EM: "something wrong with services",
+      EC: 1,
+      DT: [],
+    };
+  }
+};
 
-const createNewUser = () => { 
+const createNewUser = () => {};
 
-}
+const updateUser = () => {};
 
-const updateUser = () => { 
+const deleteAUser = async (id) => {
+  try {
+    let user = await db.User.findOne({
+      where: { id: id },
+    });
 
-}
-
-const deleteAUser = () => { 
-
-}
+    if (user) {
+      await user.destroy();
+      return {
+        EM: "Delete user success",
+        EC: 0,
+        DT: [],
+      };
+    } else {
+      return {
+        EM: "user not found",
+        EC: 1,
+        DT: [],
+      };
+    }
+  } catch (error) {
+    console.log("check error: ", error);
+    return {
+      EM: "error from userApiService",
+      EC: 1,
+      DT: [],
+    };
+  }
+};
 
 module.exports = {
-    getAllUser,
-    createNewUser,
-    updateUser,
-    deleteAUser
-}
+  getAllUser,
+  createNewUser,
+  updateUser,
+  deleteAUser,
+};
